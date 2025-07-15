bbff95b4d9302ef5bdbd825cc12eec56f528921b
Joabe Lima <77750986+Jobibit@users.noreply.github.com>
2025-07-14 22:34:57 -0300

    Update login.js

--- javascript/login.js
+++ javascript/login.js
@@ -1,16 +1,1 @@
-import { LoginController } from './controllers/LoginController.js';
-import { LoginView } from './views/LoginView.js';
-
-document.getElementById("btn-logar").addEventListener("click", () => {
-  LoginController.autenticar();
-});
-
-document.getElementById("btn-cadastrar").addEventListener("click", () => {
-  LoginView.redirecionarParaCadastro();
-});
-
-document.querySelector('.toggle-password').addEventListener("click", () => {
-  LoginView.alternarVisibilidadeSenha();
-});
-
 
